27f93b81a631c7b3a185ea8e027429ad3711c9b4
Seaya Liu <seayaliu7@gmail.com>
2024-04-02 15:18:04 -0400

    Modify logo + start backend definitions for creating accounts

--- app/src/main/java/com/example/encryptalk/CreateAccountActivity.java
+++ app/src/main/java/com/example/encryptalk/CreateAccountActivity.java
@@ -1,6 +1,8 @@
 package com.example.encryptalk;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    EditText emailInput;
+    EditText passwordInput;
+    Button createAccountBtn;
+
+    // Firebase auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,12 @@ public class CreateAccountActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        usernameInput = findViewById(R.id.create_username);
+        emailInput = findViewById(R.id.enter_email);
+        passwordInput = findViewById(R.id.create_password);
+        createAccountBtn = findViewById(R.id.create_acc_btn);
+
+        // authentication functionality starts here?
+
     }
 }
