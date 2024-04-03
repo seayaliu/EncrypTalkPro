61f83b975d0dae62a7bfe8b8ad89daef863f12b5
Fiza Sehar <115126922+fizasehar03@users.noreply.github.com>
2024-04-03 16:28:21 -0400

    updated changes for Chat fragment and Main

--- app/src/main/java/com/example/encryptalk/MainActivity.java
+++ app/src/main/java/com/example/encryptalk/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.encryptalk;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ImageButton searchButton;
+    ChatFragment chatFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        chatFragment = new ChatFragment();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        searchButton = findViewById(R.id.main_search_btn);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId()==R.id.menu_chat)
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, chatFragment).commit();
+                return true;
+            }
         });
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
+
+
+
     }
 }
