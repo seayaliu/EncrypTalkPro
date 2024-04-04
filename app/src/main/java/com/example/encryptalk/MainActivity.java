dee2fda19c122dabc2444f3dec003b79b53df9f6
Seaya Liu <seayaliu7@gmail.com>
2024-04-04 17:53:20 -0400

    Create account and login features

--- app/src/main/java/com/example/encryptalk/MainActivity.java
+++ app/src/main/java/com/example/encryptalk/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.encryptalk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -13,6 +17,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
@@ -20,10 +26,42 @@ public class MainActivity extends AppCompatActivity {
     ChatFragment chatFragment;
     ProfileFragment profileFragment;
 
+    FirebaseAuth auth;
+    FirebaseUser user;
+    TextView landing;
+    Button logoutBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        landing = findViewById(R.id.encryptalk); // temporary placeholder
+        logoutBtn = findViewById(R.id.logout_btn);
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            // landing.setText(user.getEmail()); // review this
+        }
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
 
         chatFragment = new ChatFragment();
         profileFragment = new ProfileFragment();
