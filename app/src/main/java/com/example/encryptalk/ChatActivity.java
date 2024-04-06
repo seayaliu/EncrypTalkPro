e9d58a7f5dd7fe2ea8a272b5d3bcb0d2243f1ff2
TimPokanai <timpokanai@gmail.com>
2024-04-06 17:48:01 -0400

    Finished ChatActivity

--- app/src/main/java/com/example/encryptalk/ChatActivity.java
+++ app/src/main/java/com/example/encryptalk/ChatActivity.java
@@ -1,20 +1,49 @@
 package com.example.encryptalk;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.encryptalk.model.UserInfo;
+import com.example.encryptalk.utils.AndroidUtil;
 
 public class ChatActivity extends AppCompatActivity {
+
+    UserInfo otherUser;
+    EditText messageInput;
+    ImageButton sendMessageButton;
+    ImageButton backButton;
+    TextView otherUsername;
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat);
+
+        otherUser = AndroidUtil.getUserModelFromIntent(getIntent());
+        messageInput = findViewById(R.id.chat_message_input);
+        sendMessageButton = findViewById(R.id.send_message_button);
+        backButton = findViewById(R.id.back_btn);
+        otherUsername = findViewById(R.id.other_username);
+        recyclerView = findViewById(R.id.recycler_view);
+
+        backButton.setOnClickListener((v)->{
+            onBackPressed();
+        });
+
+        otherUsername.setText(otherUser.getUsername());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
