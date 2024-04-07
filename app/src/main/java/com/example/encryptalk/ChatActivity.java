a692aca4aaf73d677a66673a293bfb5f331ffe80
Amanbeer Minhas <minhaa15@mcmaster.ca>
2024-04-07 19:46:24 -0400

    send message implemented

--- app/src/main/java/com/example/encryptalk/ChatActivity.java
+++ app/src/main/java/com/example/encryptalk/ChatActivity.java
@@ -8,19 +8,18 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.encryptalk.model.OpenChatModel;
 import com.example.encryptalk.model.UserInfo;
+import com.example.encryptalk.model.modelMessage;
 import com.example.encryptalk.utils.AndroidUtil;
 import com.example.encryptalk.utils.FireBaseUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.DocumentReference;
+
 
 import java.util.Arrays;
 
@@ -28,7 +27,7 @@ public class ChatActivity extends AppCompatActivity {
 
     UserInfo otherUser;
     String chatroomId;
-    OpenChatModel chatroomModel;
+    OpenChatModel modelMessage;
     EditText messageInput;
     ImageButton sendMessageButton;
     ImageButton backButton;
@@ -51,33 +50,51 @@ public class ChatActivity extends AppCompatActivity {
         otherUsername = findViewById(R.id.other_username);
         recyclerView = findViewById(R.id.recycler_view);
 
-        backButton.setOnClickListener((v)->{
+        backButton.setOnClickListener((v) -> {
             onBackPressed();
         });
 
-        otherUsername.setText(otherUser.getUsername());
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        sendMessageButton.setOnClickListener((v -> {
+            String message = messageInput.getText().toString().trim();
+            if (message.isEmpty())
+                return;
+            sendMessage(message);
+        }));
 
         getOrCreateOpenChatModel();
+    }
+    void sendMessage(String message){
+
+        modelMessage.setTimestamp(Timestamp.now());
+        modelMessage.setLastMsgSenderId(FireBaseUtil.currentUserId());
+
+        FireBaseUtil.getChatroomReference(chatroomId).set(modelMessage);
+
+        modelMessage modelMessage = new modelMessage(message,FireBaseUtil.currentUserId(),Timestamp.now());
+        FireBaseUtil.getChatroomMessageReference(chatroomId).add(modelMessage)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if(task.isSuccessful()){
+                            messageInput.setText("");
+
+                        }
+                    }
+                });
     }
 
     void getOrCreateOpenChatModel() {
         FireBaseUtil.getChatroomReference(chatroomId).get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
-                chatroomModel = task.getResult().toObject(OpenChatModel.class);
-                if (chatroomModel==null) {
-                    chatroomModel = new OpenChatModel(
+                modelMessage = task.getResult().toObject(OpenChatModel.class);
+                if (modelMessage ==null) {
+                    modelMessage = new OpenChatModel(
                             chatroomId,
                             Arrays.asList(FireBaseUtil.currentUserId(), otherUser.getUserId()),
                             Timestamp.now(),
                             ""
                     );
-                    FireBaseUtil.getChatroomReference(chatroomId).set(chatroomModel);
+                    FireBaseUtil.getChatroomReference(chatroomId).set(modelMessage);
                 }
             }
         });
---
+++ app/src/main/java/com/example/encryptalk/model/modelMessage.java
@@ -0,0 +1,41 @@
+package com.example.encryptalk.model;
+import com.google.firebase.Timestamp;
+
+public class modelMessage {
+    private String message;
+    private String senderId;
+    private Timestamp timestamp;
+
+    public modelMessage() {
+    }
+
+    public modelMessage(String message, String senderId, Timestamp timestamp) {
+        this.message = message;
+        this.senderId = senderId;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
